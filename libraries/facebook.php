1d9ae7b581cd4fbc37909d23ed118afe93e08b0c
Chris Harvey <chris@chrisnharvey.com>
2011-07-21 00:08:32 +0100

    Disabled cURL SSL host verification
    This fixes the error that cURL throws when trying to connect to the Face
    book API and is unable to verify the SSL certificate.

--- libraries/facebook.php
+++ libraries/facebook.php
@@ -216,6 +216,10 @@ class Facebook {
 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		
+		curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		
+		curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		
 		$response = curl_exec ($ch);
 
 		curl_close ($ch);
